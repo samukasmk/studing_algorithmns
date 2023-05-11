8078121c340c2a88cb5e13cdd942616c9671af65
Samuel Sampaio <samukasmk@gmail.com>
2023-05-11 12:32:41 -0300

    [main] Changing tests strategies

--- algorithmns/searching/binary_search/tests/test_binary_search_algo.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-@pytest.mark.parametrize("sample_test_parameter", [1, 2, 3, 4])
-def test_pytest_structure(sample_fixture, sample_test_parameter):
-    assert sample_fixture == 42
-    assert 1 <= sample_test_parameter <= 4

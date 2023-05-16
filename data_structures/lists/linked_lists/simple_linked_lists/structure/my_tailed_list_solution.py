530056db7d186e321e58f6f4ae6430811230668a
Samuel Sampaio <samukasmk@gmail.com>
2023-05-16 14:17:59 -0300

    [tailed-linked-lists] Set elements by index

--- data_structures/lists/linked_lists/simple_linked_lists/structure/my_tailed_list_solution.py
+++ data_structures/lists/linked_lists/simple_linked_lists/structure/my_tailed_list_solution.py
@@ -43,12 +43,12 @@ class SamukaLinkedList:
         for initial_element in initial_elements:
             self.append_in_tail(initial_element)
 
-    def append(self, element):
+    def append(self, element_value):
         """ Append new element as the last item if the Linked List """
-        self.append_in_tail(element)
+        self.append_in_tail(element_value)
 
-    def append_in_tail(self, element):
-        new_last_element = NodeElement(value=element)
+    def append_in_tail(self, element_value):
+        new_last_element = NodeElement(value=element_value)
         # defining the first element
         if self.head is None:
             self.head = new_last_element
@@ -59,35 +59,50 @@ class SamukaLinkedList:
         # adding more one on count
         self.__count_new_element()
 
-    def append_in_head(self, element):
-        new_first_element = NodeElement(value=element, next_node_element=self.head)
+    def append_in_head(self, element_value):
+        new_first_element = NodeElement(value=element_value, next_node_element=self.head)
         self.head = new_first_element
         # adding more one on count
         self.__count_new_element()
 
     ### get features
-    def get_element_by_index(self, index: int) -> Any:
-        """ Returns the value of desired node element by index """
+    def get_node_element_by_index(self, element_index: int) -> NodeElement:
+        """ Return the value of desired node element by index """
         if self.head is None:
             raise IndexError('This list is empty')
 
         # set initial node as head reference
-        current = self.head
+        current_node = self.head
 
         # go forward on next elements according to index received
-        for _ in range(index):
+        for _ in range(element_index):
+
             # check if current node is defined or if doesn't exist the desired index
-            if current is None:
+            if current_node is None:
                 raise IndexError(f'This list just have {self.size} elements')
+
             # go forward to the next existing node
-            current = current.next_node_element
+            current_node = current_node.next_node_element
 
+        # returns found NodeElement
+        return current_node
 
-        # returns the value
-        return current.value
+    def __getitem__(self, element_index):
+        return self.get_node_element_by_index(element_index).value
 
-    def __getitem__(self, item):
-        return self.get_element_by_index(item)
+    ### set features
+    def set_node_element_value_by_index(self, element_index: int, element_value: Any) -> None:
+        """ Change current node element for a new object """
+
+        # Convert negative index into reversive elements
+        if element_index < 0:
+            element_index = (self.size - 1) + element_index
+
+        node_element = self.get_node_element_by_index(element_index)
+        node_element.value = element_value
+
+    def __setitem__(self, element_index, element_value):
+        return self.set_node_element_value_by_index(element_index, element_value)
 
     ### insert features
 
@@ -124,19 +139,19 @@ class TestSamukaLinkedList:
         assert linked_list.tail.value == 3.6
 
     def test_append_element_in_default(self, linked_list):
-        linked_list.append(element=10)
+        linked_list.append(10)
         assert len(linked_list) == 4
         assert linked_list.tail is not None
         assert linked_list.tail.value == 10
 
     def test_append_element_in_tail(self, linked_list):
-        linked_list.append_in_tail(element=4)
+        linked_list.append_in_tail(4)
         assert len(linked_list) == 4
         assert linked_list.tail is not None
         assert linked_list.tail.value == 4
 
     def test_append_element_in_head(self, linked_list):
-        linked_list.append_in_head(element=0)
+        linked_list.append_in_head(0)
         assert len(linked_list) == 4
         assert linked_list.head is not None
         assert linked_list.head.value == 0
@@ -151,18 +166,18 @@ class TestSamukaLinkedList:
 
     @pytest.mark.parametrize("existing_index, existing_element", [(0, 1), (1, 'Second element'), (2, 3.6)])
     def test_get_existing_element_by_index(self, existing_index, existing_element, linked_list):
-        assert linked_list.get_element_by_index(existing_index) == existing_element
+        assert linked_list.get_node_element_by_index(existing_index).value == existing_element
         assert linked_list[existing_index] == existing_element
 
     def test_get_non_existing_element_in_filled_list(self, linked_list):
         with pytest.raises(IndexError):
-            linked_list.get_element_by_index(1000)
+            linked_list.get_node_element_by_index(1000)
         with pytest.raises(IndexError):
             linked_list[10000]
 
     def test_get_non_existing_element_in_empty_list(self, empty_linked_list):
         with pytest.raises(IndexError):
-            empty_linked_list.get_element_by_index(0)
+            empty_linked_list.get_node_element_by_index(0)
         with pytest.raises(IndexError):
             empty_linked_list[0]
 
@@ -173,13 +188,13 @@ class TestSamukaLinkedList:
 
     def test_set_non_existing_element_in_filled_list(self, linked_list):
         with pytest.raises(IndexError):
-            empty_linked_list.set_element_by_index(1000, 42)
+            linked_list.set_node_element_value_by_index(1000, 42)
         with pytest.raises(IndexError):
             linked_list[10000] = 42
 
     def test_set_non_existing_element_in_empty_list(self, empty_linked_list):
         with pytest.raises(IndexError):
-            empty_linked_list.set_element_by_index(0, 'non existing index')
+            empty_linked_list.set_node_element_value_by_index(0, 'non existing index')
         with pytest.raises(IndexError):
             empty_linked_list[0] = 'non existing index'
 

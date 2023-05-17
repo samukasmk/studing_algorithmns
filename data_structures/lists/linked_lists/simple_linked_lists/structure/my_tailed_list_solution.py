c7d8d1bba5529d671bc1c99d982829ab5dfac52a
Samuel Sampaio <samukasmk@gmail.com>
2023-05-17 10:56:26 -0300

    [tailed-linked-lists] Insert element in desired position

--- data_structures/lists/linked_lists/simple_linked_lists/structure/my_tailed_list_solution.py
+++ data_structures/lists/linked_lists/simple_linked_lists/structure/my_tailed_list_solution.py
@@ -6,9 +6,9 @@ class UnkwownMethodOnAppendAction(Exception):
 
 
 class NodeElement:
-    def __init__(self, value, next_node_element=None):
+    def __init__(self, value, next_node=None):
         self.value = value
-        self.next_node_element = next_node_element
+        self.next_node = next_node
 
 
 class SamukaLinkedList:
@@ -37,12 +37,14 @@ class SamukaLinkedList:
         """ Magic method for python return the length of this object """
         return self.size
 
-    ### append features
-    def __append_initial_elements(self, initial_elements: List[Any]) -> None:
-        """ Append initial values """
-        for initial_element in initial_elements:
-            self.append_in_tail(initial_element)
+    def get_positive_index(self, element_index: int) -> int:
+        print(self.size)
+        """ Convert element index to positive position """
+        if element_index < 0:
+            element_index = self.size + element_index
+        return element_index
 
+    ### append features
     def append(self, element_value):
         """ Append new element as the last item if the Linked List """
         self.append_in_tail(element_value)
@@ -54,22 +56,33 @@ class SamukaLinkedList:
             self.head = new_last_element
         # redefining the last element
         if self.tail is not None:
-            self.tail.next_node_element = new_last_element
+            self.tail.next_node = new_last_element
         self.tail = new_last_element
         # adding more one on count
         self.__count_new_element()
 
     def append_in_head(self, element_value):
-        new_first_element = NodeElement(value=element_value, next_node_element=self.head)
+        new_first_element = NodeElement(value=element_value, next_node=self.head)
         self.head = new_first_element
         # adding more one on count
         self.__count_new_element()
 
+    def __append_initial_elements(self, initial_elements: List[Any]) -> None:
+        """ Append initial values """
+        for initial_element in initial_elements:
+            self.append_in_tail(initial_element)
+
     ### get features
-    def get_node_element_by_index(self, element_index: int) -> NodeElement:
-        """ Return the value of desired node element by index """
+    def __raise_exception_if_is_empty(self) -> None:
         if self.head is None:
             raise IndexError('This list is empty')
+
+    def get_element_by_index(self, element_index: int) -> NodeElement:
+        """ Return the value of desired node element by index """
+        self.__raise_exception_if_is_empty()
+
+        # convert negative index into positive numbers
+        element_index = self.get_positive_index(element_index)
 
         # set initial node as head reference
         current_node = self.head
@@ -82,29 +95,83 @@ class SamukaLinkedList:
                 raise IndexError(f'This list just have {self.size} elements')
 
             # go forward to the next existing node
-            current_node = current_node.next_node_element
+            current_node = current_node.next_node
 
         # returns found NodeElement
         return current_node
 
     def __getitem__(self, element_index):
-        return self.get_node_element_by_index(element_index).value
+        return self.get_element_by_index(element_index).value
 
     ### set features
-    def set_node_element_value_by_index(self, element_index: int, element_value: Any) -> None:
+    def set_element_by_index(self, element_index: int, element_value: Any) -> None:
         """ Change current node element for a new object """
 
-        # Convert negative index into reversive elements
-        if element_index < 0:
-            element_index = (self.size - 1) + element_index
+        # convert negative index into positive numbers
+        element_index = self.get_positive_index(element_index)
 
-        node_element = self.get_node_element_by_index(element_index)
+        # replace value of the existing node element
+        node_element = self.get_element_by_index(element_index)
         node_element.value = element_value
 
     def __setitem__(self, element_index, element_value):
-        return self.set_node_element_value_by_index(element_index, element_value)
+        return self.set_element_by_index(element_index, element_value)
 
     ### insert features
+    def insert(self, index_position: int, element_value: Any) -> None:
+        """ Insert new element in position of an existing element """
+        self.__raise_exception_if_is_empty()
+
+        # convert negative index into positive numbers
+        index_position = self.get_positive_index(index_position)
+        print(index_position)
+        # get current and previous node
+        current_node = self.head
+        previous_node = None
+
+        for _ in range(index_position):
+            if current_node is None:
+                raise IndexError(f'This list just have {self.size} elements')
+
+            previous_node = current_node
+            current_node = current_node.next_node
+
+        # insert new node
+        new_element = NodeElement(element_value, next_node=current_node)
+
+        if index_position == 0:
+            self.head = new_element
+        else:
+            previous_node.next_node = new_element
+
+        # adding more one on count
+        self.__count_new_element()
+
+    def to_str(self):
+        """ Returns a string representation of a linked nodes in lists """
+        # append initial left bracket
+        str_object = '['
+        # count index found
+        index = 0
+        # get initial element
+        actual_node = self.head
+        while actual_node is not None:
+            # append to string the index and node __str__ of value
+            str_object += f"({index}: {actual_node.value}), "
+            actual_node = actual_node.next_node
+            # increment index count
+            index += 1
+        # remove final comma
+        str_object = str_object.rstrip(', ')
+        # append final right bracket
+        str_object += ']'
+        return str_object
+
+    def __repr__(self):
+        return self.to_str()
+
+    def __str__(self):
+        return self.to_str()
 
     ### remove features
 
@@ -122,7 +189,7 @@ def empty_linked_list():
 
 @pytest.fixture()
 def linked_list():
-    return SamukaLinkedList(initial_elements=[1, 'Second element', 3.6])
+    return SamukaLinkedList(initial_elements=['First element', 'Second element', 'Third element', 'Fourth element'])
 
 
 ### Pytest: Tests
@@ -133,51 +200,88 @@ class TestSamukaLinkedList:
         assert empty_linked_list.tail is None
 
     def test_normal_list_creation(self, linked_list):
-        assert linked_list.head.value == 1
-        assert len(linked_list) == 3
+        assert len(linked_list) == 4
+        assert linked_list.head is not None
+        assert linked_list.head.value == 'First element'
+        assert linked_list.head.next_node is not None
+        assert linked_list.head.next_node.value == 'Second element'
+        assert linked_list.head.next_node.next_node is not None
+        assert linked_list.head.next_node.next_node.value == 'Third element'
+        assert linked_list.head.next_node.next_node.next_node is not None
+        assert linked_list.head.next_node.next_node.next_node.value == 'Fourth element'
         assert linked_list.tail is not None
-        assert linked_list.tail.value == 3.6
+        assert linked_list.tail.value == 'Fourth element'
 
     def test_append_element_in_default(self, linked_list):
         linked_list.append(10)
-        assert len(linked_list) == 4
+        assert len(linked_list) == 5
         assert linked_list.tail is not None
         assert linked_list.tail.value == 10
 
     def test_append_element_in_tail(self, linked_list):
         linked_list.append_in_tail(4)
-        assert len(linked_list) == 4
+        assert len(linked_list) == 5
         assert linked_list.tail is not None
         assert linked_list.tail.value == 4
 
     def test_append_element_in_head(self, linked_list):
         linked_list.append_in_head(0)
-        assert len(linked_list) == 4
+        assert len(linked_list) == 5
         assert linked_list.head is not None
         assert linked_list.head.value == 0
 
-    def test_insert_middle_element(self):
-        linked_list.insert(position=3, element='Third position')
+    def test_insert_new_element_in_the_begin(self, linked_list):
         assert len(linked_list) == 4
+        linked_list.insert(1, 'Third position')
+        assert len(linked_list) == 5
         assert linked_list.head is not None
-        assert linked_list.head.next_node_element is not None
-        assert linked_list.head.next_node_element.next_node_element is not None
-        assert linked_list.head.next_node_element.next_node_element.value == 'Third position'
+        assert linked_list.head.next_node is not None
+        assert linked_list.head.next_node.value == 'Third position'
 
-    @pytest.mark.parametrize("existing_index, existing_element", [(0, 1), (1, 'Second element'), (2, 3.6)])
+    def test_insert_new_element_in_the_middle(self, linked_list):
+        assert len(linked_list) == 4
+        linked_list.insert(3, 'Fourth position')
+        assert len(linked_list) == 5
+        assert linked_list.head is not None
+        assert linked_list.head.next_node is not None
+        assert linked_list.head.next_node.next_node is not None
+        assert linked_list.head.next_node.next_node.next_node is not None
+        assert linked_list.head.next_node.next_node.next_node.value == 'Fourth position'
+
+    def test_insert_new_element_in_the_last_positive(self, linked_list):
+        assert len(linked_list) == 4
+        linked_list.insert(4, 'Last position')
+        assert len(linked_list) == 5
+        assert linked_list.head is not None
+        assert linked_list.head.next_node is not None
+        assert linked_list.head.next_node.next_node is not None
+        assert linked_list.head.next_node.next_node.next_node is not None
+        assert linked_list.head.next_node.next_node.next_node.next_node is not None
+        assert linked_list.head.next_node.next_node.next_node.next_node.value == 'Last position'
+
+    def test_insert_new_element_index_error(self, linked_list):
+        assert len(linked_list) == 4
+        with pytest.raises(IndexError):
+            linked_list.insert(5, 'Last position')
+
+    @pytest.mark.parametrize("existing_index, existing_element", (
+            (0, 'First element'),
+            (1, 'Second element'),
+            (2, 'Third element'),
+            (3, 'Fourth element')))
     def test_get_existing_element_by_index(self, existing_index, existing_element, linked_list):
-        assert linked_list.get_node_element_by_index(existing_index).value == existing_element
+        assert linked_list.get_element_by_index(existing_index).value == existing_element
         assert linked_list[existing_index] == existing_element
 
     def test_get_non_existing_element_in_filled_list(self, linked_list):
         with pytest.raises(IndexError):
-            linked_list.get_node_element_by_index(1000)
+            linked_list.get_element_by_index(1000)
         with pytest.raises(IndexError):
             linked_list[10000]
 
     def test_get_non_existing_element_in_empty_list(self, empty_linked_list):
         with pytest.raises(IndexError):
-            empty_linked_list.get_node_element_by_index(0)
+            empty_linked_list.get_element_by_index(0)
         with pytest.raises(IndexError):
             empty_linked_list[0]
 
@@ -188,13 +292,13 @@ class TestSamukaLinkedList:
 
     def test_set_non_existing_element_in_filled_list(self, linked_list):
         with pytest.raises(IndexError):
-            linked_list.set_node_element_value_by_index(1000, 42)
+            linked_list.set_element_by_index(1000, 42)
         with pytest.raises(IndexError):
             linked_list[10000] = 42
 
     def test_set_non_existing_element_in_empty_list(self, empty_linked_list):
         with pytest.raises(IndexError):
-            empty_linked_list.set_node_element_value_by_index(0, 'non existing index')
+            empty_linked_list.set_element_by_index(0, 'non existing index')
         with pytest.raises(IndexError):
             empty_linked_list[0] = 'non existing index'
 
@@ -211,13 +315,22 @@ class TestSamukaLinkedList:
         with pytest.raises(IndexError):
             del linked_list[10000]
 
-    @pytest.mark.parametrize("existing_elements", [1, 'Second element', 3.6])
-    def test_search_existing_element_in_list(self, existing_elements):
-        for existing_element in existing_elements:
-            assert existing_element in linked_list
+    @pytest.mark.parametrize("existing_element", (
+            'First element',
+            'Second element',
+            'Third element',
+            'Fourth element'))
+    def test_search_existing_element_in_list(self, existing_element):
+        assert existing_element in linked_list
 
     def test_search_non_existing_element_in_list(self):
         assert 'Non existing element' in linked_list is False
+
+    def test_to_str_representation(self, linked_list):
+        expected_string = '[(0: First element), (1: Second element), (2: Third element), (3: Fourth element)]'
+        assert linked_list.to_str() == expected_string
+        assert str(linked_list) == expected_string
+        assert repr(linked_list) == expected_string
 
 
 if __name__ == '__main__':

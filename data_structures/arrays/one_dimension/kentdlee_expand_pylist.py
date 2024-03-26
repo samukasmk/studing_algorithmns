9398c87617665021af1987fa1ee31a95a600d14c
Samuel Sampaio <samukasmk@gmail.com>
2024-03-25 23:33:16 -0300

    Adding simple solution

--- data_structures/arrays/one_dimension/kentdlee_expand_pylist.py
+++ data_structures/arrays/one_dimension/kentdlee_expand_pylist.py
@@ -1,12 +1,21 @@
+"""
+From book: Data Structures and Algorithms with Python
+Developed by: Kent D. Lee and Steve Hubbard
+Chapter: 4.1. - The PyList Datatype
+Where to buy the book: https://www.amazon.com.br/dp/3319130714
+Source code: https://kentdlee.github.io/CS2Plus/build/html/chap4/chap4.html#the-pylist-datatype
+"""
+
+
 class PyList:
     # The size below is an initial number of locations for the list object. The
     # count instance variable keeps track of how many elements are currently stored
     # in the list since self.items may have empty locations at the end.
-    def __init__(self,size=5):
+    def __init__(self, size=5):
         self.items = [None] * size
         self.count = 0
 
-    def append(self,item):
+    def append(self, item):
         if self.count == len(self.items):
             # We must make the list bigger by allocating a new list and copying
             # all the elements over to the new list.
@@ -15,9 +24,10 @@ class PyList:
                 newitems[k] = self.items[k]
 
             self.items = newitems
-            
+
         self.items[self.count] = item
         self.count += 1
+
 
 def main():
     p = PyList()
@@ -28,6 +38,7 @@ def main():
     print(p.items)
     print(p.count)
     print(len(p.items))
+
 
 if __name__ == "__main__":
     main()
